7c26cbf59c36c58196c2372d3a7754ebba12d716
Francisco Josue <franc1sc0.sv.xd@gmail.com>
2023-04-07 22:02:55 -0600

    comboboxPro

--- projects/08-cripto-app/src/components/Forms.jsx
+++ projects/08-cripto-app/src/components/Forms.jsx
@@ -1,10 +1,6 @@
-// import { useState } from 'react'
 import { coins, coins2 } from '../mocks/monedas'
-// import { Combobox } from '@headlessui/react'
-// import { AiOutlineCheck } from 'react-icons/ai'
-// import { BsChevronExpand } from 'react-icons/bs'
 import { useForm } from 'react-hook-form'
-import { ComboboxPro } from './Combobox'
+import { ComboboxPro } from './ComboboxPro'
 /**
  * Combobox
  * Crear un estado para guardar el valor seleccionado
@@ -19,50 +15,12 @@ export function Forms () {
   const { handleSubmit, control } = useForm()
 
   const succesSubmit = (data) => {
-    // e.preventDefault()
-    // const data = Object.fromEntries(new FormData(e.target))
     console.log(data)
   }
 
-  // const filteredCoins =
-  //   query === ''
-  //     ? coins
-  //     : coins.filter((coin) => {
-  //       return coin.name.toLowerCase().includes(query.toLowerCase())
-  //     })
   return (
     <>
       <form onSubmit={handleSubmit(succesSubmit)} className='flex flex-col gap-2'>
-        {/* <Controller
-          control={control}
-          defaultValue={selectedCoin}
-          name='coins'
-          render={({ field: { onChange } }) => (
-            <Combobox
-              value={selectedCoin} onChange={(e => {
-                onChange(e)
-                setSelectedCoin(e)
-              })}
-            >
-              <div className='relative mt-1'>
-                <div className='relative flex px-5 overflow-hidden text-left bg-white rounded-lg shadow-md cursor-default w-max h-max focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300'>
-                  <Combobox.Input
-                    onChange={(e) => setQuery(e.target.value)} displayValue={(coin) => coin.name}
-                    className='w-full py-4 pr-10 text-gray-900 border-none focus-visible:outline-none'
-                  />
-                  <Combobox.Button>
-                    <BsChevronExpand />
-                  </Combobox.Button>
-                </div>
-                <Combobox.Options className='absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                  {filteredCoins.length === 0 && query !== ''
-                    ? <NothingFound />
-                    : <RenderResultsCoins coins={filteredCoins} />}
-                </Combobox.Options>
-              </div>
-            </Combobox>
-          )}
-        /> */}
         <ComboboxPro name='coins' options={coins} control={control} />
         <ComboboxPro name='coins2' options={coins2} control={control} />
         <button type='submit' className='px-5 py-2 bg-red-300 rounded w-max'>Enviar</button>
@@ -70,22 +28,3 @@ export function Forms () {
     </>
   )
 }
-
-// function NothingFound () {
-//   return (
-//     <div className='relative px-4 py-2 text-gray-700 cursor-default select-none'>
-//       Nothing found.
-//     </div>
-//   )
-// }
-// function RenderResultsCoins ({ coins }) {
-//   return coins.map((coin, i) => (
-//     <Combobox.Option
-//       key={i} value={coin}
-//       className='relative flex items-center gap-2 px-5 py-3 cursor-default select-none ui-active:bg-blue-500 ui-active:text-white ui-not-active:bg-white ui-not-active:text-black'
-//     >
-//       <AiOutlineCheck className='hidden ui-selected:block' />
-//       <p className='pl-5 font-normal ui-selected:font-medium ui-selected:pl-0'>{coin.name}</p>
-//     </Combobox.Option>
-//   ))
-// }
